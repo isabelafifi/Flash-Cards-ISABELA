02be7479b52cf976e98cbb5ca6289b541b6dad09
isabela <silva.isabela29@escola.pr.gov.br>
2025-09-16 09:30:46 -0300

    16/09

--- perguntas.js/perguntas.js
+++ perguntas.js/perguntas.js
@@ -8,7 +8,61 @@
     
 
 criaCartao(
-    'categoria',
+    'Conhecimentos gerais',
     'Quantas patas uma formiga possui?',
     '6'
+)
+
+criaCartao(
+    'Matemática'
+'Quantos anos tem um século?',
+'100'
+)
+
+criaCartao(
+'Conhecimentos gerais',
+'Qual é o nome do único satélite natural da Terra?',
+' Lua'
+)
+
+  criaCartao(
+'Geografia',
+'Qual é o maior país da América do Sul?',
+'Brasil'
+)
+
+criaCartao(
+'Conhecimentos gerais',
+'Quem pintou o quadro Mona Lisa?',
+'Leonardo da Vinci'
+)
+
+ criaCartao(
+'Português',
+'Que nome se dá para o coletivo de lobos?',
+'Alcateia'
+)
+
+criaCartao(
+'Geografia',
+'Como é o nome da energia gerada pelo vento?',
+'Eólica'
+)
+
+criaCartao(
+'Quimíca',
+' Qual é a fórmula da água?',
+'H20'
+)
+
+criaCartao(
+'História',
+'Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?',
+'Titanic'
+)
+
+criaCartao(
+'Geografia',
+'Qual é a capital da Espanha?',
+'Madrid'
 )
